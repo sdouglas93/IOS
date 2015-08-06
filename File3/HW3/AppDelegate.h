fb7ef6e2b1d9e60736a3f8b48f6027b4d245e463
Sareenah Douglas <snd258@nyu.edu>
2015-08-05 23:06:56 -0400

    hey again

---
+++ File3/HW3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW3
+//
+//  Created by Sareenah Douglas on 3/15/15.
+//  Copyright (c) 2015 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ File3/HW3/NewViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewViewController.h
+//  HW3
+//
+//  Created by Sareenah Douglas on 3/15/15.
+//  Copyright (c) 2015 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewViewController : UIViewController
+
+@end
