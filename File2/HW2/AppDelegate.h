104254e0c1f6e866b739de772befded2c7cde2e4
Sareenah Douglas <snd258@nyu.edu>
2015-08-05 23:13:36 -0400

    Projet2

---
+++ File2/HW2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW2
+//
+//  Created by Sareenah Douglas on 3/4/15.
+//  Copyright (c) 2015 nyu.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+//@property (strong, nonatomic) NSMutableArray * identifiant;
+
+@end
+
